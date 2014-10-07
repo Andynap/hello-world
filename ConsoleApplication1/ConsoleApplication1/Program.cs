71d1032de6711ccb207765bc3967c8b2413ed6a3
Filipsi <Filip.sikula@seznam.cz>
2014-10-07 17:05:04 +0200

    Instance Žák

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConsoleApplication1 {
+
+    class Zak {
+        public string Jmeno;
+        public int Vek;
+
+        public Zak(string jmeno, int vek) {
+            Jmeno = jmeno;
+            Vek = vek;
+
+            Console.WriteLine("Byl vytvořen nový Žák, jehož jméno je {0} a věk je {1}", jmeno, vek);
         }
     }
+
+    static class Trida {
+        
+    }
+
+    static class Program {
+        static void Main(string[] args) {
+            new Zak("Filip", 19);
+
+            Console.ReadKey();
+        }
+
+    }
+
 }
